544203c18accca58769f65017d23e181d510bf11
Lassi Heikkilä <lassi.m.heikkila@gmail.com>
2022-04-13 14:39:20 +0300

    Minor change to frontpage + persist login state in localStorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import * as React from 'react';
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { BrowserRouter, Link, Routes, Route, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
 import Home from './routes/Home';
 import Tasks from './routes/Tasks';
@@ -17,36 +18,55 @@ import NotFound from './routes/NotFound';
 import LoginView from './routes/LoginView';
 import LogoutView from './routes/LogoutView';
 
-import { selectUser, selectOrg } from './state/Auth';
+import { selectToken, selectUser, selectOrg, selectRole, setToken } from './state/Auth';
 
 import './App.css';
 
 import { machines, records, users, tasks, schedules } from './data/dummy';
 
-function App() {
+const localStorageTokenKey = 'taskey-token';
 
+function App() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const token = useSelector(selectToken);
     const user = useSelector(selectUser);
     const org = useSelector(selectOrg);
+    const role = useSelector(selectRole);
+
+    // do this only on initial page load
+    useEffect(()=>{
+        const t = window.localStorage.getItem(localStorageTokenKey);
+        if (t && t !== '') {
+            dispatch(setToken(t));
+        }
+    },[]);
+
+    // save the token to local storage whenever it changes
+    useEffect(()=>{
+        window.localStorage.setItem(localStorageTokenKey, token);
+    },[token]);
 
     return (
         <Container fluid='xxl'>
             <Navbar bg='light' expand='lg'>
-                <Navbar.Brand href="/app">Taskey</Navbar.Brand>
+                <Navbar.Brand href={(token && token !== '') ? '/app' : '/'}>Taskey</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse className="justify-content-end">
-                    <Nav>
+                    <Nav activeKey='/' onSelect={(selectedKey) => navigate(selectedKey)}>
                         { (user && org) ?
                             <>
-                            <Nav.Item>
-                                <Nav.Link href='/change-pw'>Signed in as: {user} @ {org}</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="/logout">Log out</Nav.Link>
-                            </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey='/account-settings'>Signed in as: {user} @ {org}</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="/logout">Log out</Nav.Link>
+                                </Nav.Item>
                             </>
                         :
                             <Nav.Item>
-                                <Nav.Link href="/login">Log in</Nav.Link>
+                                <Nav.Link eventKey="/login">Log in</Nav.Link>
                             </Nav.Item>
                         }
                     </Nav>
@@ -55,13 +75,20 @@ function App() {
             <Routes>
                 <Route path='/login'  element={<LoginView  />} />
                 <Route path='/logout' element={<LogoutView />} />
-                <Route path='/app'    element={<Home       />}  >
-                    <Route path="/app/tasks"     element={<Tasks      tasks={tasks}          />} />
-                    <Route path="/app/schedules" element={<Schedules  schedules={schedules}  />} />
-                    <Route path="/app/machines"  element={<Machines   machines={machines}    />} />
-                    <Route path="/app/records"   element={<Records    records={records}      />} />
-                    <Route path="/app/users"     element={<Users      users={users}          />} />
-                </Route>
+                { (token && token !== '') ?
+                    <Route path='/app'    element={<Home       />}  >
+                        <Route path="/app/tasks"     element={<Tasks      tasks={tasks}          />} />
+                        <Route path="/app/schedules" element={<Schedules  schedules={schedules}  />} />
+                        <Route path="/app/machines"  element={<Machines   machines={machines}    />} />
+                        <Route path="/app/records"   element={<Records    records={records}      />} />
+                        <Route path="/app/users"     element={<Users      users={users}          />} />
+                    </Route>
+                 :
+                    <>
+                        <Route index element={<div><p>Taskey is a task scheduling service. You can use it to remotely perform actions on connected machines.</p></div>} />
+                        <Route path="*" element={<div><p>You must log in to use the application</p></div> } />
+                    </>
+                }
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </Container>
